af4326164fadbddf3c35296a7291b7b83f2d73d4
Razchek <ryan.irecki@gmail.com>
2013-08-05 01:38:34 +1200

    ...

---
+++ EEComponents/KerbalSeatEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kerbtown.EEComponents
+{
+    public class KerbalSeatEx : KerbalSeat
+    {
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            if (Occupant != null)
+            {
+                
+            }
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+        }
+    }
+}
