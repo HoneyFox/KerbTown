9a6b8982ad9e8500ff5b701a1f2f3297be418f0a
HoneyFox <HoneyFox@fake.fake>
2014-03-30 00:59:47 +0800

    Finally found the ultimate way...

--- KerbTown.cs
+++ KerbTown.cs
@@ -19,7 +19,7 @@ using Debug = UnityEngine.Debug;
 
 namespace Kerbtown
 {
-    [KSPAddon(KSPAddon.Startup.Flight, false)]
+	[KSPAddonFixed(KSPAddon.Startup.SpaceCentre, true, typeof(KerbTown))]
     public partial class KerbTown : MonoBehaviour
     {
         private string _currentBodyName = "";
@@ -1183,4 +1183,39 @@ namespace Kerbtown
             }
         }
     }
+
+	    /// <summary>
+    /// KSPAddon with equality checking using an additional type parameter. Fixes the issue where AddonLoader prevents multiple start-once addons with the same start scene.
+    /// By Majiir
+    /// </summary>
+    public class KSPAddonFixed : KSPAddon, IEquatable<KSPAddonFixed>
+    {
+        private readonly Type type;
+
+        public KSPAddonFixed(KSPAddon.Startup startup, bool once, Type type)
+            : base(startup, once)
+        {
+            this.type = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) { return false; }
+            return Equals((KSPAddonFixed)obj);
+        }
+
+        public bool Equals(KSPAddonFixed other)
+        {
+            if (this.once != other.once) { return false; }
+            if (this.startup != other.startup) { return false; }
+            if (this.type != other.type) { return false; }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.startup.GetHashCode() ^ this.once.GetHashCode() ^ this.type.GetHashCode();
+        }
+    }
 }
+
